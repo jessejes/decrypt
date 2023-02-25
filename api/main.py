4c457311ebcd21442c806c34f146dcbcdadff29b
jesse <jesse.bakenhus@gmx.de>
2023-02-25 14:57:20 +0100

    Restructured project

--- api/main.py
+++ api/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
+import routers.authentication
+
 app = FastAPI()
+
+app.include_router(routers.authentication.router)
+
 
 @app.get("/")
 def hello_world():
-	return {"Hello": "World"}
-
+    return {"Hello": "asd"}
